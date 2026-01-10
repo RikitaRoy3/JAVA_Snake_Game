996349f67f9fe8772f7b1439b2a867bf3fac85cd
Rikita Roy <rikitaroy21@gmail.com>
2026-01-11 03:09:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A Basic Snake game , coded using Java ! ☕ 
+
+( 🐍: " hisss, Lets test your gamming skills ,now.." )
---
+++ src/App.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardwidth = 600;
+        int boardheight = 600;
+
+        JFrame frame = new JFrame("Snake Game");
+        frame.setSize(boardwidth, boardheight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+
+        Snake snake = new Snake(boardwidth, boardheight);
+
+        frame.add(snake);
+        frame.setContentPane(snake);
+        frame.pack();
+        frame.setVisible(true);
+        snake.requestFocus();
+    }
+}
